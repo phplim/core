77d4600447c6e98e41f60fa83e73606564421b3c
Wayren <wayrenc@gmail.com>
2021-12-27 15:56:47 +0800

    1640591807

--- src/function.php
+++ src/function.php
@@ -46,7 +46,7 @@ function lim_mkdir($dirs='',$base='')
  * @param    integer                  $t [description]
  * @return   [type]                      [description]
  */
-function uc($k, $v = false, $t = 0)
+function uc($k=null, $v = false, $t = 0)
 {
     if (!Server::$cache) {
         Server::$cache = new \Yac();
@@ -57,7 +57,16 @@ function uc($k, $v = false, $t = 0)
         return Server::$cache->set($k, $v, $t);
     }
 
-    return Server::$cache->get($k);
+    if ($k) {
+        return Server::$cache->get($k);
+    }
+
+    return Server::$cache;   
+}
+
+function rc($db = null)
+{
+    
 }
 
 function ti($fn)
